2f0d458e7cca2b7836756fd277c83e8c7ce9964d
anapereira <ana.she.po@gmail.com>
2024-11-25 13:02:37 +0100

    Updated

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -19,12 +19,27 @@ class MainActivity : AppCompatActivity() {
         val btncalculate = findViewById<Button>(R.id.button_calculate)
 
         btncalculate.setOnClickListener {
-            val height = inputheight.text
-            val weight = inputweight.text
+            val height: Float = inputheight.text.toString().toFloat()
+            val weight: Float = inputweight.text.toString().toFloat()
 
-            println(weight)
-            println(height)
+            val height2 = height*(height)
+            val IMC = weight/height2
 
+            if (IMC < 18.5){
+                println("Underweight")
+            }
+            if (IMC >= 18.5 && IMC <= 24.9){
+                println("Normal")
+            }
+            if (IMC >= 25.0 && IMC <= 29.9){
+                println("Overweight")
+            }
+            if (IMC >= 30.0 && IMC <= 39.9){
+                println("Obesity")
+            }
+            if (IMC >= 40.0){
+                println("Severe obesity")
+            }
         }
     }
 }
